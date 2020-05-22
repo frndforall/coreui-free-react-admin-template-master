b7f432a9c80c25ff199f8779c3b3bc12894422e8
Nagendra Sharma <nagendra.sharma@cybersoft.net>
2020-05-22 11:45:25 +0530

    Initial commit

---
+++ src/views/Theme/Template/template.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';  
+
+class Template extends React.Component {
+    render() {
+        return (
+
+                <h1>This is a sample template Tag to display a route</h1>
+
+        );
+    }
+}
+
+export default Template;
