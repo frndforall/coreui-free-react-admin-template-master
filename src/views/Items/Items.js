13724c6d3c08895f35efff563881f6825e18136e
Nagendra sharma <nagendra.sharma@cybersoft.net>
2020-05-22 20:36:57 +0530

    Update Items.js

--- src/views/Items/Items.js
+++ src/views/Items/Items.js
@@ -17,13 +17,28 @@ function UserRow(props) {
   }
 
   return (
-    <tr key={item.id.toString()}>
-      <th scope="row"><Link to={itemLink}>{item.id}</Link></th>
-      <td><Link to={itemLink}>{item.name}</Link></td>
-      <td>{item.registered}</td>
-      <td>{item.type}</td>
-      <td><Link to={itemLink}><Badge color={getBadge(item.status)}>{item.status}</Badge></Link></td>
-    </tr>
+    // <tr key={item.id.toString()}>
+    //   <th scope="row"><Link to={itemLink}>{item.id}</Link></th>
+    //   <td><Link to={itemLink}>{item.name}</Link></td>
+    //   <td>{item.registered}</td>
+    //   <td>{item.type}</td>
+    //   <td><Link to={itemLink}><Badge color={getBadge(item.status)}>{item.status}</Badge></Link></td>
+    // </tr>
+        <td>
+            <Card>
+              <CardHeader>
+                  Item Id: <Link to={itemLink}>{item.id}</Link>
+              </CardHeader>
+              <CardBody>
+               Item Name: <Link to={itemLink}>{item.name}</Link><br/>
+              Item Registered: {item.registered}<br/>
+              Item Status: <Link to={itemLink}><Badge color={getBadge(item.status)}>{item.status}</Badge></Link>
+                {/* Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. */}
+              </CardBody>
+            </Card>
+            </td>
   )
 }
 
@@ -36,14 +51,14 @@ class Items extends Component {
     return (
       <div className="animated fadeIn">
         <Row>
-          <Col xl={6}>
+          <Col xl={20}>
             <Card>
               <CardHeader>
                 <i className="fa fa-align-justify"></i> Users <small className="text-muted">example</small>
               </CardHeader>
               <CardBody>
                 <Table responsive hover>
-                  <thead>
+                  {/* <thead>
                     <tr>
                       <th scope="col">id</th>
                       <th scope="col">name</th>
@@ -51,11 +66,13 @@ class Items extends Component {
                       <th scope="col">type</th>
                       <th scope="col">status</th>
                     </tr>
-                  </thead>
+                  </thead> */}
                   <tbody>
+                    <tr>
                     {itemList.map((item, index) =>
                       <UserRow key={index} item={item}/>
                     )}
+                    </tr>
                   </tbody>
                 </Table>
               </CardBody>
